a5113cced9ad5805457906209f2a29334bd01fb7
Goutam-Parmar <145038183+Goutam-Parmar@users.noreply.github.com>
2024-12-31 08:23:51 +0000

    new file

---
+++ JS_Tutorial/DAY2.js
@@ -0,0 +1,24 @@
+console.log("Hey!");
+const acoount_ID = 12221;
+//acoount_ID = 3
+console.log(acoount_ID);
+var Email = "goutamParmar@gmail.com"
+console.log(Email);
+let address = "Delhi"
+console.log(address);
+city = "Greater Noida"
+console.log(city);
+id = 4
+console.log(id);
+
+Email = "goutam@gmail.com"
+console.log(Email);
+
+address = "INDORE"
+console.log(address);
+
+city = "Greater"
+console.log(city);
+
+let state;
+console.log(state);
